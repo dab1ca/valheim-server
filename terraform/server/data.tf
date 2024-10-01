0ff9ba9ff5aeec6aeed088a76c24a91316147abe
dab1ca <45315505+dab1ca@users.noreply.github.com>
2024-10-01 17:06:07 +0300

    Update data.tf

--- terraform/server/data.tf
+++ terraform/server/data.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "core" {
   backend = "azurerm"
   config = {
-    resource_group_name  = "ttsv-rg" # Valheim backend resource group
-    storage_account_name = "ttsvsauksouth" # Valheim backend storage account
+    resource_group_name  = "" # Valheim backend resource group
+    storage_account_name = "" # Valheim backend storage account
     container_name       = "valheim"
     key                  = "core.tfstate"
   }
